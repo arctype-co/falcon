e2b524a3382c9d09ea69774e8f7d5e2bba9c7367
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-08-23 14:36:54 -0700

    Add consistent riak backends

--- container/riak-kv/cluster-init.sh
+++ container/riak-kv/cluster-init.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Create backend bucket types
-# riak-admin bucket-type create bitcask_backend '{"props":{"backend":"bitcask"}}'
+## Create backend bucket types
 riak-admin bucket-type create leveldb_backend '{"props":{"backend":"leveldb"}}'
+riak-admin bucket-type create leveldb_consistent '{"props":{"backend": "leveldb", "consistent":true}}'
 riak-admin bucket-type create memory_backend '{"props":{"backend":"memory"}}'
-# riak-admin bucket-type activate bitcask_backend
+riak-admin bucket-type create memory_consistent '{"props":{"backend":"memory", "consistent": true}}'
+
+## Activate bucket types
 riak-admin bucket-type activate leveldb_backend
+riak-admin bucket-type activate leveldb_consistent
 riak-admin bucket-type activate memory_backend
+riak-admin bucket-type activate memory_consistent
