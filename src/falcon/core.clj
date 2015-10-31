4699d800f3df600512dfbbd6c56f03c5bba933a4
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-10-31 12:45:53 -0700

    Argument count verification

--- src/falcon/core.clj
+++ src/falcon/core.clj
@@ -4,10 +4,12 @@
   [args fn-def]
   (let [fn-args (second fn-def)
         fn-body (drop 2 fn-def)
+        fn-arg-names (vec (map str fn-args))
         bindings (->> (for [i (range (count fn-args))]
-                        [(nth fn-args i) `(nth args i)])
+                        [(nth fn-args i) `(nth ~args ~i)])
                       (reduce concat)
-                      (vec))
-        bindings [(first fn-args) `(first ~args)
-                  (second fn-args) `(second ~args)]]
-    `(let ~bindings ~@fn-body)))
+                      (vec))]
+    #_(println bindings)
+    `(try (let ~bindings ~@fn-body)
+          (catch js/Error e#
+            (println "Expected arguments:" ~fn-arg-names)))))
