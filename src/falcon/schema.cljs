a8ce8771d6ad4aaa7917714ab216957f21612a20
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-12-01 19:43:15 -0800

    Update default local config

--- src/falcon/schema.cljs
+++ src/falcon/schema.cljs
@@ -15,28 +15,31 @@
 (def VagrantBaseIp
   (S/pred (fn [addr] (= 4 (count (string/split addr #"\."))))))
 
+(def BaseClusterConfig
+  {(S/required-key :kube-server) S/Str}) ; ip:port
+
 (def VagrantClusterConfig
-  {(S/required-key :provider) (S/enum "vagrant")
-   (S/required-key :nodes) S/Int
-   (S/required-key :coreos-channel) S/Str
-   (S/required-key :master-mem-mb) S/Int
-   (S/required-key :master-cpus) S/Int
-   (S/required-key :node-mem-mb) S/Int
-   (S/required-key :node-cpus) S/Int
-   (S/required-key :kube-ui) S/Bool
-   (S/optional-key :dockercfg) S/Str 
-   ;(S/optional-key :base-ip) VagrantBaseIp ; buggy, not working
-   })
+  (merge BaseClusterConfig
+         {(S/required-key :provider) (S/enum "vagrant")
+          (S/required-key :nodes) S/Int
+          (S/required-key :coreos-channel) S/Str
+          (S/required-key :master-mem-mb) S/Int
+          (S/required-key :master-cpus) S/Int
+          (S/required-key :node-mem-mb) S/Int
+          (S/required-key :node-cpus) S/Int
+          (S/required-key :kube-ui) S/Bool
+          (S/optional-key :dockercfg) S/Str 
+          ;(S/optional-key :base-ip) VagrantBaseIp ; buggy, not working
+          }))
 
 (def UbuntuClusterConfig
-  {(S/required-key :provider) (S/enum "ubuntu")
-   (S/required-key :kube-server) S/Str ; ip:port
-   (S/required-key :install-env) {S/Keyword S/Str}})
+  (merge BaseClusterConfig
+         {(S/required-key :provider) (S/enum "ubuntu")
+          (S/required-key :install-env) {S/Keyword S/Str}}))
 
 (def NativeClusterConfig
-  {(S/required-key :provider) (S/enum "native")
-   (S/required-key :kube-server) S/Str ; ip:port
-   })
+  (merge BaseClusterConfig
+         {(S/required-key :provider) (S/enum "native")}))
 
 (def ClusterConfig
   (S/conditional #(= (:provider %) "vagrant") VagrantClusterConfig
