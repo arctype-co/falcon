3a78bf299a536b0bec66512d5c9bde0b3f84a673
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-01-28 19:30:05 -0800

    Add config add, remove, status commands

--- src/falcon/cmd/config.cljs
+++ src/falcon/cmd/config.cljs
@@ -1,5 +1,5 @@
 (ns falcon.cmd.config
-  (:refer-clojure :exclude [do])
+  (:refer-clojure :exclude [do name remove])
   (:require
     [clojure.string :as string]
     [cljs.core.async :as async :refer [<!]]
@@ -13,16 +13,35 @@
     [falcon.core :refer [require-arguments]]
     [cljs.core.async.macros :refer [go]]))
 
+(S/defn add
+  "Add new config repository"
+  [{:keys [git-branch]} args]
+  (require-arguments
+    args
+    (fn [git-url name]
+      (shell/passthru (concat ["git" "clone" git-url name])
+                      {:cwd (core/cloud-path)}))))
+
+(S/defn remove
+  "Remove a config repository"
+  [{:keys [git-branch]} args]
+  (require-arguments
+    args
+    (fn [repository]
+      (let [dir (core/cloud-path repository)]
+        (go 
+          (println "Warning: this will remove" dir)
+          (<! (core/safe-wait))
+          (<! (shell/passthru (concat ["rm" "-rf" dir]))))))))
+
 (S/defn pull
   "Pull latest configurations"
   [{:keys [git-branch]} args]
   (require-arguments
     args
     (fn [repository]
-      (let [dir (core/cloud-path repository)]
-        (println dir)
-        (shell/passthru (concat ["git" "pull" "origin" git-branch])
-                        {:cwd dir})))))
+      (shell/passthru (concat ["git" "pull" "origin" git-branch])
+                      {:cwd (core/cloud-path repository)}))))
 
 (S/defn push
   "Push configurations"
@@ -33,9 +52,21 @@
       (shell/passthru (concat ["git" "push" "origin" git-branch])
                       {:cwd (core/cloud-path repository)}))))
 
+(S/defn status
+  "Status of configuration"
+  [opts args]
+  (require-arguments
+    args
+    (fn [repository]
+      (shell/passthru (concat ["git" "status"])
+                      {:cwd (core/cloud-path repository)}))))
+
 (def cli
   {:doc "Configuration management"
    :options
    [["-b" "--git-branch" "Git branch <branch>" "Git tag or branch" :default "master"]]
-   :commands {"pull" pull
-              "push" push}})
+   :commands {"add" add
+              "remove" remove
+              "pull" pull
+              "push" push
+              "status" status}})
