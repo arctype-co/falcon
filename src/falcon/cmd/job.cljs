cb095b17da2ff5678e7a15c1fed7aa7eee85a481
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-04-01 23:07:25 -0700

    Implement job delete command

--- src/falcon/cmd/job.cljs
+++ src/falcon/cmd/job.cljs
@@ -6,9 +6,12 @@
     [cljs.pprint :refer [pprint]]
     [cljs.tools.cli :as cli]
     [schema.core :as S]
+    [falcon.core :as core]
+    [falcon.config :as config-ns]
     [falcon.schema :as schema]
     [falcon.shell :as shell]
-    [falcon.shell.kubectl :as kubectl])
+    [falcon.shell.kubectl :as kubectl]
+    [falcon.template :as template])
   (:require-macros
     [falcon.core :refer [require-arguments]]
     [cljs.core.async.macros :refer [go]]))
@@ -18,9 +21,39 @@
   [opts args]
   (require-arguments
     args
-    (fn [pod-name]
-      (go 
-        (<! (kubectl/run opts "create" "job" pod-name))))))
+    (fn [service]
+      (let [controller-tag (core/new-tag)]
+        (core/do-all-profiles opts (core/profiles opts service)
+          (fn [opts]
+            (let [{:keys [container-tag]} (config-ns/service opts service)
+                  container-tag (or (:container-tag opts) container-tag)
+                  params {:service service
+                          :controller-tag controller-tag
+                          :container-tag container-tag}]
+              (core/print-summary "Create job:" opts params)
+              (go
+                (<! (template/make-yml "job.yml" opts params))
+                (<! (-> (kubectl/run opts "create" "-f" (core/species-path service "job.yml"))
+                        (shell/check-status)))))))))))
+
+(S/defn delete
+  "Cancel a job. Buggy - goes into infinite loop when job is failed."
+  [opts args]
+  (require-arguments 
+    args
+    (fn [service controller-tag]
+      (core/do-all-profiles opts (core/profiles opts service)
+        (fn [opts]
+          (let [{:keys [container-tag]} (config-ns/service opts service)
+                container-tag (or (:container-tag opts) container-tag)
+                params {:service service
+                        :controller-tag controller-tag
+                        :container-tag container-tag}]
+            (core/print-summary "Delete job:" opts params)
+            (go
+              (when-not (:yes opts) (<! (core/safe-wait)))
+              (<! (template/make-yml "job.yml" opts params))
+              (<! (kubectl/run opts "delete" "-f" (core/species-path service "job.yml"))))))))))
 
 (S/defn list
   "List jobs"
@@ -33,4 +66,5 @@
    :options 
    [["-e" "--environment <env>" "Environment"]]
    :commands {"create" create
+              "delete" delete
               "list" list}})
