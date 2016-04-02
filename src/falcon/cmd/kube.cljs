fbda288510ad726c8838d23c2f2e214948530f7e
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-04-01 19:10:04 -0700

    Rm redundant 'kube' commands

--- src/falcon/cmd/kube.cljs
+++ src/falcon/cmd/kube.cljs
@@ -13,12 +13,6 @@
     [falcon.core :refer [require-arguments]]
     [cljs.core.async.macros :refer [go]]))
 
-(S/defn env
-  "Get environments"
-  [opts args]
-  (go
-    (<! (kubectl/run opts "get" "namespaces"))))
-
 (S/defn logs
   "Get logs"
   [{:keys [follow] :as opts} args]
@@ -31,30 +25,6 @@
                           [node])]
           (<! (apply kubectl/run opts "logs" kube-args)))))))
 
-(S/defn nodes
-  "Get nodes status"
-  [opts args]
-  (go
-    (<! (kubectl/run opts "get" "nodes"))))
-
-(S/defn pods
-  "Get pods status"
-  [opts args]
-  (go
-    (<! (kubectl/run opts "get" "pods"))))
-
-(S/defn rc
-  "Get replication controllers status"
-  [opts args]
-  (go
-    (<! (kubectl/run opts "get" "rc"))))
-
-(S/defn services
-  "Get services status"
-  [opts args]
-  (go
-    (<! (kubectl/run opts "get" "services"))))
-
 (S/defn sh
   "Launch a shell in a pod"
   [opts args]
@@ -63,15 +33,6 @@
     (fn [pod]
       (go 
         (<! (kubectl/run opts "exec" "-i" "--tty" pod "sh"))))))
-
-(S/defn delete-pod
-  "Delete a pod"
-  [opts args]
-  (require-arguments
-    args
-    (fn [pod]
-      (go 
-        (<! (kubectl/run opts "delete" "pod" pod))))))
 
 (S/defn do
   "Do a kubernetes command"
