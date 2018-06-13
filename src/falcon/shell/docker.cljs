81557b3254c294acebe18b402fa2b88221d5d584
Ryan Sundberg <ryan.sundberg@gmail.com>
2018-06-13 12:48:33 -0700

    Add --pull option to pull latest image on container build

--- src/falcon/shell/docker.cljs
+++ src/falcon/shell/docker.cljs
@@ -5,14 +5,17 @@
     [falcon.shell :as core]))
 
 (def Options
-  {(S/optional-key :no-cache) S/Bool})
+  {(S/optional-key :no-cache) S/Bool
+   (S/optional-key :pull) S/Bool})
 
 (S/defn run :- schema/Chan
   [command :- S/Str
-   {:keys [no-cache]} :- Options
+   {:keys [no-cache
+           pull]} :- Options
    args :- [S/Str]]
   (let [flags (cond-> []
-                no-cache (conj "--no-cache=true"))]
+                no-cache (conj "--no-cache")
+                pull (conj "--pull"))]
     (core/passthru (concat ["docker" command] flags args))))
 
 (def build (partial run "build"))
