78f5454b9f4e796323bc8db269f2bb40aa993042
Ryan Sundberg <ryan.sundberg@gmail.com>
2020-02-01 00:44:14 -0800

    Fix issues with relative docker paths

--- src/falcon/cmd/container.cljs
+++ src/falcon/cmd/container.cljs
@@ -48,7 +48,7 @@
                       (assoc :working-dir dockerpath))
                     [(str "-t=" container-id)
                      (if (some? dockerpath)
-                       dockerpath
+                       "."
                        (species-path container))])
                   (shell/check-status)))
           (<! (-> (docker/tag
